399ecb967a4f882a92d13a70ca4739e5a8940a9a
Anton Borries <anton.borries@gmail.com>
2021-06-27 20:15:17 +0200

    Add Integration Tests

---
+++ example/integration_test/android_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:home_widget/home_widget.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  final testData = <String, dynamic>{
+    'stringKey': 'stringValue',
+    'intKey': 12,
+    'boolKey': true,
+    'floatingNumberKey': 12.1,
+    'nullValueKey': null,
+  };
+
+  final defaultValue = MapEntry('defaultKey', 'defaultValue');
+
+  setUpAll(() {
+    // Clear all Data
+    for(final key in testData.keys) {
+      HomeWidget.saveWidgetData(key, null);
+    }
+  });
+
+  group('Test Data operations', () {
+    for(final testSet in testData.entries) {
+      testWidgets('Test ${testSet.value?.runtimeType}', (tester) async {
+        // Save Data
+        await HomeWidget.saveWidgetData(testSet.key, testSet.value);
+
+        final retrievedData = await HomeWidget.getWidgetData(testSet.key);
+        expect(retrievedData, testSet.value);
+      });
+    }
+
+    testWidgets('Returns default Value', (tester) async {
+      final returnValue = await HomeWidget.getWidgetData(defaultValue.key, defaultValue: defaultValue.value);
+
+      expect(returnValue, defaultValue.value);
+    });
+  });
+}
---
+++ example/integration_test/ios_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:home_widget/home_widget.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Need Group Id', () {
+    testWidgets('Save Data needs GroupId', (tester)  async {
+      expect(() async => await HomeWidget.saveWidgetData('AnyId', null), throwsException);
+    });
+  });
+  
+  group('With Group Id', () {
+    final testData = <String, dynamic>{
+      'stringKey': 'stringValue',
+      'intKey': 12,
+      'boolKey': true,
+      'floatingNumberKey': 12.1,
+      'nullValueKey': null,
+    };
+
+    final defaultValue = MapEntry('defaultKey', 'defaultValue');
+
+    setUpAll(() async {
+      // Add Group Id
+      await HomeWidget.setAppGroupId('group.es.antonborri.integrationtest');
+      // Clear all Data
+      for (final key in testData.keys) {
+        await HomeWidget.saveWidgetData(key, null);
+      }
+    });
+
+    group('Test Data operations', () {
+      for (final testSet in testData.entries) {
+        testWidgets('Test ${testSet.value?.runtimeType}', (tester) async {
+          // Save Data
+          await HomeWidget.saveWidgetData(testSet.key, testSet.value);
+
+          final retrievedData = await HomeWidget.getWidgetData(testSet.key);
+          expect(retrievedData, testSet.value);
+        });
+      }
+
+      testWidgets('Returns default Value', (tester) async {
+        final returnValue = await HomeWidget.getWidgetData(defaultValue.key,
+            defaultValue: defaultValue.value);
+
+        expect(returnValue, defaultValue.value);
+      });
+    });
+  });
+}
