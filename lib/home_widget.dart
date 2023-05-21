e9c46582387c7edd6964c82f516eaed89e02202c
Leigha Jarett <leigha.jarett@gmail.com>
2023-05-21 17:16:53 -0400

    Adding method to render flutter widget to image (#126)
    * Adding method to render flutter widget to image

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
-import 'dart:ui';
+import 'dart:io';
+import 'dart:ui' as ui;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:home_widget/home_widget_callback_dispatcher.dart';
+import 'package:flutter/rendering.dart';
+import 'package:path_provider_foundation/path_provider_foundation.dart';
 
 /// A Flutter Plugin to simplify setting up and communicating with HomeScreenWidgets
 class HomeWidget {
   static const MethodChannel _channel = MethodChannel('home_widget');
   static const EventChannel _eventChannel = EventChannel('home_widget/updates');
+
+  /// The AppGroupId used for iOS Widgets
+  static String? groupId;
 
   /// Save [data] to the Widget Storage
   ///
@@ -55,6 +62,7 @@ class HomeWidget {
   /// Required on iOS to set the AppGroupId [groupId] in order to ensure
   /// communication between the App and the Widget Extension
   static Future<bool?> setAppGroupId(String groupId) {
+    HomeWidget.groupId = groupId;
     return _channel.invokeMethod('setAppGroupId', {'groupId': groupId});
   }
 
@@ -92,9 +100,114 @@ class HomeWidget {
   /// More Info on setting this up in the README
   static Future<bool?> registerBackgroundCallback(Function(Uri?) callback) {
     final args = <dynamic>[
-      PluginUtilities.getCallbackHandle(callbackDispatcher)?.toRawHandle(),
-      PluginUtilities.getCallbackHandle(callback)?.toRawHandle()
+      ui.PluginUtilities.getCallbackHandle(callbackDispatcher)?.toRawHandle(),
+      ui.PluginUtilities.getCallbackHandle(callback)?.toRawHandle()
     ];
     return _channel.invokeMethod('registerBackgroundCallback', args);
+  }
+
+  /// Generate a screenshot based on a given widget.
+  /// This method renders the widget to an image (png) file with the provided filename.
+  /// The png file is saved to the App Group container and the full path is returned as a string.
+  /// The filename is saved to UserDefaults using the provided key.
+  static Future renderFlutterWidget(
+    Widget widget, {
+    String fileName = 'screenshot',
+    String key = 'filename',
+    double? pixelRatio,
+    Size? logicalSize,
+  }) async {
+    /// finding the widget in the current context by the key.
+    final RenderRepaintBoundary repaintBoundary = RenderRepaintBoundary();
+
+    /// create a new pipeline owner
+    final PipelineOwner pipelineOwner = PipelineOwner();
+
+    /// create a new build owner
+    final BuildOwner buildOwner = BuildOwner(focusManager: FocusManager());
+
+    try {
+      final RenderView renderView = RenderView(
+        view: ui.PlatformDispatcher.instance.implicitView!,
+        child: RenderPositionedBox(
+          alignment: Alignment.center,
+          child: repaintBoundary,
+        ),
+        configuration: ViewConfiguration(
+          size: logicalSize ?? Size.zero,
+          devicePixelRatio: 1.0,
+        ),
+      );
+
+      /// setting the rootNode to the renderview of the widget
+      pipelineOwner.rootNode = renderView;
+
+      /// setting the renderView to prepareInitialFrame
+      renderView.prepareInitialFrame();
+
+      /// setting the rootElement with the widget that has to be captured
+      final RenderObjectToWidgetElement<RenderBox> rootElement =
+          RenderObjectToWidgetAdapter<RenderBox>(
+        container: repaintBoundary,
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: Column(
+            // image is center aligned
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              widget,
+            ],
+          ),
+        ),
+      ).attachToRenderTree(buildOwner);
+
+      ///adding the rootElement to the buildScope
+      buildOwner.buildScope(rootElement);
+
+      ///adding the rootElement to the buildScope
+      buildOwner.buildScope(rootElement);
+
+      /// finialize the buildOwner
+      buildOwner.finalizeTree();
+
+      ///Flush Layout
+      pipelineOwner.flushLayout();
+
+      /// Flush Compositing Bits
+      pipelineOwner.flushCompositingBits();
+
+      /// Flush paint
+      pipelineOwner.flushPaint();
+
+      final ui.Image image =
+          await repaintBoundary.toImage(pixelRatio: pixelRatio!);
+
+      /// The raw image is converted to byte data.
+      final ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+
+      final PathProviderFoundation provider = PathProviderFoundation();
+
+      try {
+        final String? directory = await provider.getContainerPath(
+          appGroupIdentifier: HomeWidget.groupId!,
+        );
+        final String path = '$directory/homeWidget/$fileName.png';
+        final File file = File(path);
+        await file.writeAsBytes(byteData!.buffer.asUint8List());
+
+        // Save the filename to UserDefaults if a key was provided
+        _channel.invokeMethod<bool>('saveWidgetData', {
+          'id': key,
+          'data': path,
+        });
+
+        return path;
+      } catch (e) {
+        throw Exception('Failed to save screenshot to app group container: $e');
+      }
+    } catch (e) {
+      throw Exception('Failed to render the widget: $e');
+    }
   }
 }
