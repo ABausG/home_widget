f113631daa1a1f1a43287e15c0c9c8800959f578
Boros Máté <33902750+qwadrox@users.noreply.github.com>
2024-03-10 14:16:35 +0100

    feat: ✨ dart - get installed widgets information

---
+++ lib/home_widget_info.dart
@@ -0,0 +1,38 @@
+/// Represents information about the pinned home widget.
+class HomeWidgetInfo {
+  /// Only iOS. The size of the widget: small, medium, or large.
+  String? family;
+
+  /// Only iOS. The string specified during creation of the widget’s configuration.
+  String? kind;
+
+  /// Only Android. Unique identifier for each instance of the widget, used for tracking individual widget usage.
+  int? widgetId;
+
+  /// Only Android. The [androidClassName] parameter represents the class name of the widget.
+  String? androidClassName;
+
+  /// Only Android. Loads the localized label to display to the user in the AppWidget picker.
+  String? label;
+
+  /// Constructs a [HomeWidgetInfo] object.
+  HomeWidgetInfo({this.family, this.kind, this.widgetId, this.androidClassName, this.label});
+
+  /// Constructs a [HomeWidgetInfo] object from a map.
+  ///
+  /// The [data] parameter is a map that contains the widget information.
+  factory HomeWidgetInfo.fromMap(Map<String, dynamic> data) {
+    return HomeWidgetInfo(
+      family: data['family'] as String?,
+      kind: data['kind'] as String?,
+      widgetId: data['widgetId'] as int?,
+      androidClassName: data['androidClassName'] as String?,
+      label: data['label'] as String?,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'HomeWidgetInfo{family: $family, kind: $kind, widgetId: $widgetId, androidClassName: $androidClassName, label: $label}';
+  }
+}
