28c74936f34f78c1473cff47cd25e3403b4f1a4b
Anton Borries <anton.borries@gmail.com>
2024-07-16 23:29:52 +0200

    test: add test cases for long and big double for android

--- example/integration_test/android_test.dart
+++ example/integration_test/android_test.dart
@@ -10,7 +10,9 @@ void main() {
     'intKey': 12,
     'boolKey': true,
     'floatingNumberKey': 12.1,
+    'largeDoubleKey': double.infinity,
     'nullValueKey': null,
+    'longKey': DateTime(0).millisecondsSinceEpoch,
   };
 
   const defaultValue = MapEntry('defaultKey', 'defaultValue');
@@ -24,7 +26,7 @@ void main() {
 
   group('Test Data operations', () {
     for (final testSet in testData.entries) {
-      testWidgets('Test ${testSet.value?.runtimeType}', (tester) async {
+      testWidgets('Test ${testSet.key}', (tester) async {
         // Save Data
         await HomeWidget.saveWidgetData(testSet.key, testSet.value);
 
