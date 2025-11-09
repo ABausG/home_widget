f0e75191d23615356ea55b8c9abaaafbc575eaa7
Anton Borries <anton.borries@gmail.com>
2025-11-09 12:50:18 +0100

    docs: Add Example For iOS Lockscreen Widgets (#380)

---
+++ examples/lockscreen_widgets/ios/Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Anton Borries on 09.11.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LockScreenWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LockScreenWidget()
+    }
+}
