33b8b4203803e831be5e7a348558ce7694f24faa
Anton Borries <anton.borries@gmail.com>
2026-09-17 11:27:29 +0200

    ci: Use yaml based codemagic workflow for ios integration tests (#457)

--- packages/home_widget/example/ios/Runner/AppDelegate.swift
+++ packages/home_widget/example/ios/Runner/AppDelegate.swift
@@ -4,13 +4,11 @@ import home_widget
 import workmanager
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60 * 15))
 
     WorkmanagerPlugin.setPluginRegistrantCallback { registry in
@@ -24,5 +22,9 @@ import workmanager
     }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
